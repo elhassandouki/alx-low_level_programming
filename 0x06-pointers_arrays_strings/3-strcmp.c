a5f8886956b2f1211af7747581f54452feeadb1f
elhassandouki <elhassandouki@gmail.com>
2023-03-30 18:15:42 -0700

    tasks

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * _strncpy - a function that print n elem of array
+ * _strcmp - a function that print n elem of array
  * @s1: pointer variable
  * @s2: variable
  * Return: void return nothing
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-
-	while (s1[i] && s2[j])
+	while (*s1 == *s2)
 	{
-	if (s1[i] > s2[j])
-		return ((unsigned char)s1[i] - s2[j]);
-	else if (s1[i] < s2[j])
-		return  ((unsigned char)s1[i] - s2[j]);
-		i++, j++;
+		if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
