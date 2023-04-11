8f2eed63f089deae7297aa5bd11a1665f02928f1
elhassandouki <elhassandouki@gmail.com>
2023-04-10 18:51:01 -0700

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - The Function
+ * @argc:  The variable
+ * @argv: The Variable
+ * Return: The Return value/void
+ */
+
 int main(int argc,char *argv[] __attribute__((unused)) ){
 	printf("%d\n", argc-1);
 	return(0)
