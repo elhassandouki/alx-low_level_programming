d2db5ba723888bd37bd8e2c0a9364dc237e8ae09
elhassandouki <elhassandouki@gmail.com>
2023-04-10 20:06:16 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,27 +2,53 @@
 #include <stdlib.h>
 
 /**
+ * is_valid_number - The Function
+ * @s:  The variable
+ * Return: The Return value/void
+ */
+
+
+int is_valid_number(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (!(s[i] >= '0' && s[i] <= '9'))
+			return (0);
+		i++;
+	}
+	return (1);
+
+}
+
+/**
  * main  - The Function
  * @argc:  The variable
  * @argv: The variable
  * Return: The Return value/void
  */
 
-int main(int argc,char **argv){ 
-	int sum =0; 
-	char *i; 
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int sum = 0;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	while(--argc){ 
-		for(i[argc];*i;i++){ 
-			if(*i < '0' || *i>  '9') 
-				return(printf("Error\n",),1);
-		       sum += atoi(argv[argc]);	
+	while (argv[i])
+	{
+		if (!is_valid_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
 		}
+		sum += atoi(argv[i]);
+		i++;
 	}
-	printf("%d\n",sum);
-       	return (0); 
+	printf("%d\n", sum);
+	return (0);
 }
