68efd26a597510fd5f2c72a8ed6f884dc8db8c8e
elhassandouki <elhassandouki@gmail.com>
2023-04-10 19:29:35 -0700

    task 00

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,13 @@
 #include <stdio>
 #include <unistd.h>
 
+/**
+ * main - The Function
+ * @argc:  The variable
+ * @argv: THe variable
+ * Return: The Return value/void
+ */
+
 int main(int argc,char const *argv[]){
 	int i=0;
 	while(argc--){
