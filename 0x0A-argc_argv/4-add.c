1f6696613f56d694f745fd170c09be77843ff9a3
elhassandouki <elhassandouki@gmail.com>
2023-04-10 20:00:20 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,11 @@
 int main(int argc,char *argv[]){ 
 	int sum =0; 
 	char *i; 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 	while(--argc){ 
 		for(i[argc];*i;i++){ 
 			if(*i < '0' || *i>  '9') 
