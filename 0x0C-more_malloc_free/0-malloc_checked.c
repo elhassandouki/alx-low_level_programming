bf7591885541e14bbe745c0a3c2e867510828da5
elhassandouki <elhassandouki@gmail.com>
2023-04-13 19:18:24 -0700

    tasks

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - The Function
+ * @b:  The variable
+ * Return: The Return value/void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *memory;
+
+	memory = malloc(b);
+	if (!memory)
+		exit(98);
+	return (memory);
+
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _calloc - The Function
+ * @nmemb:  The variable
+ * @size:  The variable
+ * Return: The Return value/void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
+	while (i < nmemb * size)
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
