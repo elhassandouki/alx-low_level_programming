565885dc9160ca579fc4e0b5bdab338db41f43a4
elhassandouki <elhassandouki@gmail.com>
2023-04-18 18:57:08 -0700

    task 01

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ *init_dog - initializes a struct
+ *@d: the struct
+ *@name: the  value
+ *@age: the value
+ *@owner: the owner value
+ *Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ *free_dog - initializes a struct
+ *@d: the struct
+ *Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+
+}
