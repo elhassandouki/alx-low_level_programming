84bb353922b0fed10fcf4069e5caf671b9f4e993
elhassandouki <elhassandouki@gmail.com>
2023-05-02 11:32:40 -0700

    task 6 7 8

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Entry point
+ * @head: pointer to pointer listint_t
+ * Return: int
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int i;
+
+	if (!head || !*head)
+		return (0);
+
+	i = (*head)->n;
+	node = (*head)->next;
+	free(*head);
+	*head = node;
+
+
+	return (i);
+}
